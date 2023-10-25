981901795d1bb55d5da913cea3c60e3675dfad6e
Thi2030 <frontback.full@gmail.com>
2023-10-25 18:31:10 -0300

    main

--- Java_SpringBoot/src/main/java/Java_SpringBoot/domain/repositorio/AlunoRepositorio.java
+++ Java_SpringBoot/src/main/java/Java_SpringBoot/domain/repositorio/AlunoRepositorio.java
@@ -1,4 +1,4 @@
-package Java_SpringBoot.RegistroAluno;
+package Java_SpringBoot.domain.repositorio;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
